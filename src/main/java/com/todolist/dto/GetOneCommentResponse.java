2d277bae86d7ecbe07b27937f46bad2fd2c1f66b
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2025-11-04 15:40:26 +0900

    [Level6] 일정 단건 조회 댓글 포함 기능 추가, 다건 조회 응답용 DTO 클래스 분리해 생성

---
+++ src/main/java/com/todolist/dto/GetOneCommentResponse.java
@@ -0,0 +1,21 @@
+package com.todolist.dto;
+
+import com.todolist.entity.Comment;
+import lombok.Getter;
+
+@Getter
+public class GetOneCommentResponse {
+    private final Long id;
+    private final String username;
+    private final String content;
+    private final String createdAt;
+    private final String modifiedAt;
+
+    public GetOneCommentResponse(Comment comment) {
+        this.id = comment.getId();
+        this.username = comment.getUsername();
+        this.content = comment.getContent();
+        this.createdAt = comment.getCreatedAt().toString();
+        this.modifiedAt = comment.getModifiedAt().toString();
+    }
+}
