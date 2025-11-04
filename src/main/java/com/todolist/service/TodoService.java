5a25a8ca62bb07313b83187f661d69cfcb60e173
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2025-11-04 10:41:53 +0900

    [Level4] 중복 처리를 공통 private 메서드로 분리

--- src/main/java/com/todolist/service/TodoService.java
+++ src/main/java/com/todolist/service/TodoService.java
@@ -15,6 +15,17 @@ import java.util.Objects;
 public class TodoService {
     private final TodoRepository todoRepository;
 
+    //---- 유틸 메서드 -----
+    private Todo findTodoOrException(Long todoId) {
+        return todoRepository.findById(todoId).orElseThrow(
+                ()-> new IllegalArgumentException("존재하지 않는 글입니다.")
+        );
+    }
+    private void validatePassword(Todo todo, String password) {
+        if(!Objects.equals(todo.getPassword(), password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+    }
     @Transactional
     public CreateTodoResponse save(CreateTodoRequest request) {
 
@@ -57,9 +68,7 @@ public class TodoService {
     }
     @Transactional(readOnly = true)
     public GetOneTodoResponse findOne(Long todoId) {
-        Todo todo = todoRepository.findById(todoId).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 글 입니다.")
-        );
+        Todo todo = findTodoOrException(todoId);
         return new GetOneTodoResponse(
                 todo.getId(),
                 todo.getUsername(),
@@ -72,14 +81,9 @@ public class TodoService {
 
     @Transactional
     public UpdateTodoResponse update(Long todoId, UpdateTodoRequest request) {
-        Todo todo = todoRepository.findById(todoId).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 글입니다.")
-        );
+        Todo todo = findTodoOrException(todoId);
         // 비밀번호 검증
-        if (!todo.getPassword().equals(request.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
-
+        validatePassword(todo, request.getPassword());
         // 선택적 수정
         if (request.getTitle() != null && !request.getTitle().isBlank()) {
             todo.updateTitle(request.getTitle());
@@ -96,11 +100,8 @@ public class TodoService {
     }
     @Transactional
     public void delete(Long todoId, DeleteTodoRequest request) {
-        Todo todo = todoRepository.findById(todoId).orElseThrow(
-                () -> new IllegalArgumentException("존재하지않는 글입니다."));
-        if(!Objects.equals(todo.getPassword(), request.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
+        Todo todo = findTodoOrException(todoId);
+        validatePassword(todo, request.getPassword());
         todoRepository.deleteById(todoId);
     }
 }
